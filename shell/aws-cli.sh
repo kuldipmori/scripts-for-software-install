e38b1eb6989a998a2b109be521f98d58cb3a3ed0
kuldipmori <morikuldip62@gmail.com>
2023-07-25 07:19:37 +0000

    remove comment

--- shell/aws-cli.sh
+++ shell/aws-cli.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear 
 # Update the package lists
 sudo apt update
 
@@ -13,26 +14,3 @@ else
     echo "AWS CLI installation failed."
     exit 1
 fi
-
-# Configure AWS CLI with your AWS Access Key ID, Secret Access Key, and default region
-#echo "Please enter your AWS Access Key ID: "
-#read aws_access_key_id
-
-# Prompt for AWS Secret Access Key without echoing the input
-#stty -echo
-#echo "Please enter your AWS Secret Access Key: "
-#read aws_secret_access_key
-#stty echo
-#echo
-
-#echo "Please enter your default AWS region (e.g., us-east-1): "
-#read aws_default_region
-
-#aws configure set aws_access_key_id "$aws_access_key_id"
-#aws configure set aws_secret_access_key "$aws_secret_access_key"
-#aws configure set default.region "$aws_default_region"
-
-#echo "AWS CLI configuration completed."
-
-# Verify the configuration by listing S3 buckets
-# aws s3 ls
